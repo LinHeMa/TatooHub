991a0a9a9326a8942535fc6e6276290b3514cb1d
linhema <linhema0921@gmail.com>
2024-08-23 15:04:48 +0800

    feat(login): :lipstick: login form

---
+++ app/login/_components/login-form.tsx
@@ -0,0 +1,75 @@
+"use client";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function LoginForm() {
+  // TODO
+  const handleGoogleLogin = () => console.log("click google");
+  // TODO
+  const handleFacebookLogin = () => console.log("click facebook");
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account, Or you can use:
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <Button
+            onClick={handleGoogleLogin}
+            variant="outline"
+            className="w-full"
+          >
+            Login with Google
+          </Button>
+          <Button
+            onClick={handleFacebookLogin}
+            variant="outline"
+            className="w-full"
+          >
+            Login with Facebook
+          </Button>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link href="#" className="ml-auto inline-block text-sm underline">
+                Forgot your password?
+              </Link>
+            </div>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Don&apos;t have an account?{" "}
+          <Link href="#" className="underline">
+            Sign up
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
